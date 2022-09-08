48670e8a18f10681b84f98b6bfc74daf7f09d17d
Homayoon Ranjbar <horanj.1022@gmail.com>
2022-09-07 22:50:44 -0500

    Update pilot.py

--- pilot.py
+++ pilot.py
@@ -1,1 +1,114 @@
+# import libraries
+try:
+  # %tensorflow_version only exists in Colab.
+  !pip install tf-nightly
+except Exception:
+  pass
+import tensorflow as tf
+import pandas as pd
+from tensorflow import keras
+!pip install tensorflow-datasets
+import tensorflow_datasets as tfds
+import numpy as np
+import matplotlib.pyplot as plt
 
+print(tf.__version__)
+
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+# get data files
+!wget https://cdn.freecodecamp.org/project-data/sms/train-data.tsv
+!wget https://cdn.freecodecamp.org/project-data/sms/valid-data.tsv
+
+train_file_path = "train-data.tsv"
+test_file_path = "valid-data.tsv"
+
+train_df = pd.read_csv(train_file_path, sep='\t', header=None, names=['type', 'msg'])
+val, _ = train_df['type'].factorize()
+train_df['type'] = val
+test_df = pd.read_csv(test_file_path, sep='\t', header=None, names=['type', 'msg'])
+val, _ = test_df['type'].factorize()
+test_df['type'] = val
+
+train_labels = train_df['type'].values
+train_ds = tf.data.Dataset.from_tensor_slices((train_df['msg'].values, train_labels))
+test_labels = test_df['type'].values
+test_ds = tf.data.Dataset.from_tensor_slices((test_df['msg'].values, test_labels))
+
+BUFFER_SIZE = 100
+BATCH_SIZE = 32
+train_ds = train_ds.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+test_ds = test_ds.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+
+vec = TextVectorization(
+    output_mode='int',
+    max_tokens=1000,
+    output_sequence_length=1000
+)
+vec.adapt(train_ds.map(lambda text, label : text))
+
+model = tf.keras.Sequential([
+                             vec,
+                             tf.keras.layers.Embedding(
+                                 len(vec.get_vocabulary()),
+                                 64,
+                                 mask_zero=True
+                             ),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(1)
+])
+model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(1e-4),
+    metrics=['accuracy'],
+)
+
+history = model.fit(
+    train_ds,
+    validation_data=test_ds,
+    validation_steps=30,
+    epochs=10,
+)
+
+test_loss, test_acc = model.evaluate(test_ds)
+
+# function to predict messages based on model
+# (should return list containing prediction and label, ex. [0.008318834938108921, 'ham'])
+def predict_message(pred_text):
+  prediction = model.predict([pred_text])
+  prob = prediction[0][0]
+  return ([prob, 'ham' if prob < .5 else 'spam'])
+
+pred_text = "how are you doing today?"
+
+prediction = predict_message(pred_text)
+print(prediction)
+
+# Run this cell to test your function and model. Do not modify contents.
+def test_predictions():
+  test_messages = ["how are you doing today",
+                   "sale today! to stop texts call 98912460324",
+                   "i dont want to go. can we try it a different day? available sat",
+                   "our new mobile video service is live. just install on your phone to start watching.",
+                   "you have won £1000 cash! call to claim your prize.",
+                   "i'll bring it tomorrow. don't forget the milk.",
+                   "wow, is your arm alright. that happened to me one time too"
+                  ]
+
+  test_answers = ["ham", "spam", "ham", "spam", "spam", "ham", "ham"]
+  passed = True
+
+  for msg, ans in zip(test_messages, test_answers):
+    prediction = predict_message(msg)
+    if prediction[1] != ans:
+      passed = False
+
+  if passed:
+    print("You passed the challenge. Great job!")
+  else:
+    print("You haven't passed yet. Keep trying.")
+
+test_predictions()
